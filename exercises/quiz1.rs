9a0915a90de85f23dfd1262f8d0a4650f5213557
Armin Varshokar <arminvarshokar@gmail.com>
2023-09-02 21:18:47 -0400

    Progress

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    let dynamic_price = if (quantity > 40) { 1 } else { 2 };
+
+    let total_price = dynamic_price * quantity;
+
+    total_price
+}
 
 // Don't modify this function!
 #[test]
